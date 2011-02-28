c8efb300f216c49388699ef149a6ed8fc74f0a5c
mzp <mzpppp@gmail.com>
2011-02-28 23:52:54 +0900

    mark関数を検証した

--- gc.c
+++ gc.c
@@ -1,11 +1,72 @@
+typedef int bool;
+#define NULL 0
+const bool true = 1;
+const bool false = 1;
+
+struct Object;
+
+typedef struct Node{
+  struct Object* value;
+  struct Node* next;
+} Node;
+
 typedef struct Object{
-  void* value;
-  struct Object* next;
+  Node* children;
+  bool marked;
 } Object;
 
-static int index = 0;
-static Object roots[100];
 
+/*@
+  inductive In(Object* obj, Node* xs) {
+    case InEq:
+      \forall Object* obj, Node* xs;
+      xs->value == obj ==> In(obj, xs);
+   case InNext:
+      \forall Object* obj, Node* xs;
+      In(obj, xs->next) ==> In(obj, xs);
+  }
 
+  inductive Reachable(Object* o1, Object* o2) {
+    case Same:
+      \forall Object* o;
+      Reachable(o,o);
+    case ChildrenLeft:
+      \forall Object* o1, Object* o2;
+      In(o2, o1->children) ==> Reachable(o1, o2);
+  }
+ */
 
+void mark_phase(){
+}
 
+/*@
+  requires \valid(object);
+  requires (\forall Object* o; Reachable(object, o) ==> (o->marked == false));
+
+  behavior unmark:
+    requires (object->marked == false);
+    ensures (\forall Object* o; Reachable(object, o) ==> (o->marked == false));
+
+  behavior mark:
+   requires object->marked == true;
+   ensures (\forall Object* o; Reachable(object, o) ==> (o->marked == true));
+ */
+void mark(Object* object){
+  if(object->marked == false){
+    return;
+  }else{
+    Node* node = object->children;
+
+    object->marked = true;
+
+    while(node){
+      mark(node->value);
+      node = node->next;
+    }
+  }
+}
+
+void sweep_phase(){}
+
+void new_obj(int size){
+}

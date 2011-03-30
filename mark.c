5dcf741a9cbda710fdb91c9d9269cca3dc5ad3bf
mzp <mzpppp@gmail.com>
2011-03-30 22:29:41 +0900

    [from now] 2011年 3月30日 水曜日 22時29分41秒 JST
    diff --git a/mark.c b/mark.c
    index 5d9b11c..dced29e 100644
    --- a/mark.c
    +++ b/mark.c
    @@ -1,33 +1,12 @@
     #include "object.h"
    -/*
    -  // xs contains obj.
    -  inductive In(Object* obj, Node* xs) {
    -    case InEq:
    -      \forall Object* obj, Node* xs;
    -      xs->value == obj ==> In(obj, xs);
    -   case InNext:
    -      \forall Object* obj, Node* xs;
    -      In(obj, xs->next) ==> In(obj, xs);
    -  }
    -
    -  // o1->next->next->...->next = o2
    +/*@
       inductive Reachable(Object* o1, Object* o2) {
         case Same:
           \forall Object* o;
           Reachable(o,o);
         case Children:
    -      \forall Object* o1, Object* o2, Object* o3;
    -      In(o3, o1->children) ==> Reachable(o3, o2) ==> Reachable(o1, o2);
    -  }
    -
    -  inductive ValidNode{L}(Node* node) {
    -    case VNil{L}:
    -      ValidNode(NULL);
    -    case VCons{L}:
    -      \forall Node* node;
    -         \valid(node)        &&
    -         \valid(node->value) &&
    -	 ValidNode(node->value->children) ==> ValidNode{L}(node->next) ==> ValidNode(node);
    +      \forall Object* o1, Object* o2, unsigned int i;
    +      0 <= i && i < o1->size ==> Reachable(o1->children + i, o2) ==> Reachable(o1, o2);
       }
    
       // the children of marked object should be marked. See also http://pastebin.com/xGyfGXx9
    @@ -36,28 +15,31 @@
       predicate MarksAll(Object* object) = \forall Object* o, Object* s; Reachable(object, o) ==> o->marked==true;
      */
    
    -/*
    +/*@
       requires \valid(object);
    -  requires ValidNode(object->children);
    +  requires \valid_range(object->children, 0, object->size + 1);
    +
       requires Consistent(object);
    
       behavior marked:
         assumes object->marked == true;
         ensures MarksAll(object);
    -    ensures object->marked == true;
    
       behavior unmarked:
         assumes object->marked == false;
         ensures MarksAll(object);
    -    ensures object->marked == true;
    
       disjoint behaviors;
      */
     void mark(Object* object){
       if(object->marked == false){
    -    int i = 0;
    +    unsigned int i = 0;
         object->marked = true;
    
    +    /*@
    +      loop invariant \valid(object->children+i) && i <= object->size;
    +      loop variant object->size - i;
    +     */
         for(i = 0; i < object->size; i++){
           mark(object->children+i);
         }

--- mark.c
+++ mark.c
@@ -1,33 +1,12 @@
 #include "object.h"
-/*
-  // xs contains obj.
-  inductive In(Object* obj, Node* xs) {
-    case InEq:
-      \forall Object* obj, Node* xs;
-      xs->value == obj ==> In(obj, xs);
-   case InNext:
-      \forall Object* obj, Node* xs;
-      In(obj, xs->next) ==> In(obj, xs);
-  }
-
-  // o1->next->next->...->next = o2
+/*@
   inductive Reachable(Object* o1, Object* o2) {
     case Same:
       \forall Object* o;
       Reachable(o,o);
     case Children:
-      \forall Object* o1, Object* o2, Object* o3;
-      In(o3, o1->children) ==> Reachable(o3, o2) ==> Reachable(o1, o2);
-  }
-
-  inductive ValidNode{L}(Node* node) {
-    case VNil{L}:
-      ValidNode(NULL);
-    case VCons{L}:
-      \forall Node* node;
-         \valid(node)        &&
-         \valid(node->value) &&
-	 ValidNode(node->value->children) ==> ValidNode{L}(node->next) ==> ValidNode(node);
+      \forall Object* o1, Object* o2, unsigned int i;
+      0 <= i && i < o1->size ==> Reachable(o1->children + i, o2) ==> Reachable(o1, o2);
   }
 
   // the children of marked object should be marked. See also http://pastebin.com/xGyfGXx9
@@ -36,28 +15,31 @@
   predicate MarksAll(Object* object) = \forall Object* o, Object* s; Reachable(object, o) ==> o->marked==true;
  */
 
-/*
+/*@
   requires \valid(object);
-  requires ValidNode(object->children);
+  requires \valid_range(object->children, 0, object->size + 1);
+
   requires Consistent(object);
 
   behavior marked:
     assumes object->marked == true;
     ensures MarksAll(object);
-    ensures object->marked == true;
 
   behavior unmarked:
     assumes object->marked == false;
     ensures MarksAll(object);
-    ensures object->marked == true;
 
   disjoint behaviors;
  */
 void mark(Object* object){
   if(object->marked == false){
-    int i = 0;
+    unsigned int i = 0;
     object->marked = true;
 
+    /*@
+      loop invariant \valid(object->children+i) && i <= object->size;
+      loop variant object->size - i;
+     */
     for(i = 0; i < object->size; i++){
       mark(object->children+i);
     }
